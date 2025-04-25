3d223689300a2d4e72cfa29419f1601317ac9f2a
kingson4wu <kingson4wu@gmail.com>
2025-04-25 19:03:36 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/mbti-test-app/' : '/',
+  base: mode === 'production' ? '/' : '/',
   plugins: [vue()]
 }))
